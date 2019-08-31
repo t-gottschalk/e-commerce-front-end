b8bafbd3bc6baf6636b219a9b8e60aaa6d74c5b5
code-and-technology <tony.gottschalk@alumni.unc.edu>
2019-08-31 12:49:52 -0400

    Separated Component structure, finished homepage, and completed styling.

---
+++ src/components/MenuItem/MenuItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './MenuItem.scss';
+
+const MenuItem = ({ title, imageUrl, size }) => (
+		<div className={`${size} menu-item`}>
+			<div 
+				className='background-image'
+				style={{
+					backgroundImage: `url(${imageUrl})`
+				}}
+			/>
+			<div className='content'>
+				<h1 className='title'>{title.toUpperCase()}</h1>
+				<span className='subtitle'>Shop</span>
+			</div>
+		</div>
+);
+
+export default MenuItem;

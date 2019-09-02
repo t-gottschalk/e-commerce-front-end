8a040bb7d4b55a205c286e64cd855294eb87e1d2
code-and-technology <tony.gottschalk@alumni.unc.edu>
2019-09-01 21:25:49 -0400

    integrating react-dom-router withRouter()

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './MenuItem.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-		<div className={`${size} menu-item`}>
-			<div 
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+		<div 
+			className={`${size} menu-item`} 
+			onClick={() => history.push(`${match.url}${linkUrl}`)}>
+			<image
 				className='background-image'
 				style={{
 					backgroundImage: `url(${imageUrl})`
@@ -16,4 +19,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
 		</div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
